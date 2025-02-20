2a1fef9c65db0fe3db9fdd067d64854e4a4b3d75
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2025-02-19 23:27:57 -0600

    principal

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/login')
+def login():
     return render_template('login.html')
 
 @app.route('/')
-def home():
+def principal():
     return render_template('principal.html')
 
 if __name__ == '__main__':
