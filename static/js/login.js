3545c7cb97a60c0bd08073f9976c14fe160955fc
IsmaCoro <ismaelcm18182@gmail.com>
2025-02-20 01:50:46 -0600

    Mas de Login

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,13 @@
-const container = document.querySelector('.container');
+const contenedor = document.querySelector('.container');
 const loginBtn = document.querySelector('.login-btn');
 
+// Agregar un evento al botón de inicio de sesión
 loginBtn.addEventListener('click', (event) => {
     event.preventDefault(); // Evita el envío del formulario
-    container.classList.add('active'); // Agrega la clase para el efecto
-    window.location.href = '/menu'; // Redirige a menu.html
+    contenedor.classList.add('active'); // Agrega la clase para el efecto
+
+    // Espera 600 ms (el tiempo de la animación) antes de redirigir
+    setTimeout(() => {
+        window.location.href = '/menu'; // Redirige a menu.html
+    }, 600); // Este valor debe coincidir con la duración de la animación
 });
