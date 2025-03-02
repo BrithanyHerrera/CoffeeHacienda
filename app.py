c2fee476c2b481238f0247b4ac1da0a35337572b
IsmaCoro <ismaelcm18182@gmail.com>
2025-03-01 22:57:37 -0600

    Rutas y Htmls

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
-@app.route('/')
-def Salir():
-    return render_template('login.html')
+@app.route('/salir')
+def salir():
+    return redirect(url_for('login'))
 
 @app.route('/sidebar')
 def sidebar():
@@ -19,21 +19,33 @@ def bienvenida():
     return render_template('bienvenida.html')
 
 @app.route('/menu')
-def principal():
+def menu():
     return render_template('menu.html')
 
-@app.route('/finalizarOrden')
-def finalizar_orden():
-    return render_template('finalizarOrden.html')
+@app.route('/gestionProductos')
+def gestion_productos():
+    return render_template('gestionProductos.html', productos=productos)
+
+@app.route('/ordenes')
+def ordenes():
+    return render_template('ordenes.html')
+
+@app.route('/historial')
+def historial():
+    return render_template('historial.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html')
+
+@app.route('/propinas')
+def propinas():
+    return render_template('propinas.html')
 
 # Datos de ejemplo
 productos = [
     {'id': 1, 'nombre': 'Cappuccino', 'precio': 4.98}
 ]
-
-@app.route('/gestionProductos')
-def gestionProductos():
-    return render_template('gestionProductos.html', productos=productos)
 
 @app.route('/gestionProductos/agregar', methods=['POST'])
 def agregar_producto():
@@ -43,13 +55,13 @@ def agregar_producto():
         'precio': float(request.form['precio'])
     }
     productos.append(nuevo_producto)
-    return redirect(url_for('listar_productos'))
+    return redirect(url_for('gestion_productos'))
 
 @app.route('/gestionProductos/eliminar/<int:id>')
 def eliminar_producto(id):
     global productos
     productos = [p for p in productos if p['id'] != id]
-    return redirect(url_for('listar_productos'))
+    return redirect(url_for('gestion_productos'))
 
 if __name__ == '__main__':
     app.run(debug=True)
