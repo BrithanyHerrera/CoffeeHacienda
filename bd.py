a67ac2f0a6594a60efe1e5404deafbfcd509f2ff
IsmaCoro <ismaelcm18182@gmail.com>
2025-03-08 00:16:20 -0600

    GG

--- bd.py
+++ bd.py
@@ -8,8 +8,8 @@ load_dotenv()
 def Conexion_BD():
     try:
         connection = pymysql.connect(
-            host=os.getenv('DB_HOST', 'localhost'),  # Cambia localhost si es necesario
-            port=int(os.getenv('DB_PORT', 3307)),   # Usar el puerto correcto
+            host=os.getenv('DB_HOST', 'localhost'),
+            port=int(os.getenv('DB_PORT', 3307)),
             user=os.getenv('DB_USER', 'root'),
             password=os.getenv('DB_PASSWORD', 'root'),
             database=os.getenv('DB_NAME', 'puntodeventa'),
