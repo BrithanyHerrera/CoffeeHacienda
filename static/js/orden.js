c9c8cdfcb1b5ccd028fa02bb2f4df763de0fcef4
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2025-03-15 00:59:16 -0600

    ORDENES, BUSCADOR POR CLIETE Y ACOMODO DE CSS

--- static/js/orden.js
+++ static/js/orden.js
@@ -38,3 +38,36 @@ function cerrarDetallesOrden() {
     let modal = document.getElementById('ordenModal');
     modal.style.display = 'none';
 }
+
+function buscarVentas() {
+    const nombreCliente = document.getElementById('buscarCliente').value.toLowerCase();
+
+    console.log("Cliente buscado:", nombreCliente);
+
+    // Selecciona todas las filas de la tabla
+    const filas = document.querySelectorAll('.listaOrdenes tbody tr');
+    filas.forEach(fila => {
+        const clienteNombre = fila.querySelector('td:nth-child(1)').textContent.toLowerCase();
+
+        console.log("Cliente actual:", clienteNombre);
+
+        // Compara si el nombre del cliente coincide con lo que se ha ingresado en el buscador
+        if (!nombreCliente || clienteNombre.includes(nombreCliente)) {
+            fila.style.display = ''; // Muestra la fila
+        } else {
+            fila.style.display = 'none'; // Oculta la fila
+        }
+    });
+}
+
+function reestablecerFiltros() {
+    document.getElementById('buscarCliente').value = '';
+
+    // Muestra todas las filas nuevamente
+    const filas = document.querySelectorAll('.listaOrdenes tbody tr');
+    filas.forEach(fila => {
+        fila.style.display = ''; // Muestra todas las filas
+    });
+}
+
+
